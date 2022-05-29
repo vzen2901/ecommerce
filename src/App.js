62603686f815098107de9382a5b61853d1d633f9
vzen2901 <vzen2901@gmail.com>
2022-05-29 15:52:34 +0700

    form-input

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Switch,Route } from 'react-router-dom';
 
 import './App.css';
 
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -15,6 +16,7 @@ function App() {
         <Switch>
           <Route exact path="/" component = {HomePage} />
           <Route exact path='/shop' component = {ShopPage} />
+          <Route exact path='/signin' component = {SignInAndSignUpPage} />
         </Switch>
     </div>
   );
