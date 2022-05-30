5ca98760bbda7b32f0fc39fb74afd5fd7b6e8c48
vzen2901 <vzen2901@gmail.com>
2022-05-30 07:08:17 +0700

    sign-in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
 import './sign-in.styles.scss';
 
@@ -27,25 +29,25 @@ class SignIn extends React.Component {
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
                 <form onSubmit = {this.handleSubmit}>
-                    <input 
+                    <FormInput
                         name = 'email' 
                         type = 'email' 
+                        label = 'email'
                         value = {this.state.email} 
-                        onChange = {this.handleChange} 
+                        handleChange = {this.handleChange} 
                         required 
                     />
-                    <label>Email</label>
 
-                    <input 
+                    <FormInput
                         name = 'password' 
                         type = 'password' 
+                        label = 'password'
                         value = {this.state.password} 
-                        onChange = {this.handleChange} 
+                        handleChange = {this.handleChange} 
                         required 
                     />
-                    <label>Password</label>
 
-                    <input type = 'submit' value = 'Submit Form' />
+                    <CustomButton type = 'submit'> Sign In </CustomButton>
                 </form>
             </div>
         );
