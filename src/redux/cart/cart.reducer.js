0c3e4b1a9936d4f909bae7194694e7cba6bd771b
vzen2901 <vzen2901@gmail.com>
2022-07-27 14:47:59 +0700

    upload data shop

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,8 @@
 import CartActionTypes from "./cart.types";
+import { addItemToCart } from "./cart-utils";
+
+
+
 const INITIAL_STATE = {
     hidden: true,
     cartItems: []
@@ -14,11 +18,11 @@ const cartReducer = (state = INITIAL_STATE,action) => {
         case CartActionTypes.ADD_ITEM:
             return {
                 ...state,
-                cartItems: [...state.cartItems, action.payload]
+                // cartItems: [...state.cartItems, action.payload]
+                cartItems: addItemToCart(state.cartItems, action.payload)
             };
         default:
             return state;
-
     }
 };
 export default cartReducer;
